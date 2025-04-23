64403e8b763e12076ade14b7b1e2104e47e9350d
zubair <zzubairahmed402@gmail.com>
2025-04-23 14:02:45 +0500

    cors

--- main.py
+++ main.py
@@ -77,8 +77,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:5001",
-        "https://final-ff.onrender.com",
-        "https://final-app-deploy-2.onrender.com"
+        "https://final-ff.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
